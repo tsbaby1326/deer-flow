d8428787fd76147503fd97c44993af2e3f77c084
tsbaby1326 <imported-history@users.noreply.github.com>
2026-01-16 09:58:16 +0800

    fix: navigate to home only in open-mode

--- frontend/src/components/workspace/workspace-header.tsx
+++ frontend/src/components/workspace/workspace-header.tsx
@@ -8,27 +8,27 @@ import { cn } from "@/lib/utils";
 export function WorkspaceHeader({ className }: { className?: string }) {
   const { state } = useSidebar();
   return (
-    <Link href="/workspace">
-      <div
-        className={cn(
-          "group/workspace-header flex h-15 flex-col justify-center",
-          className,
-        )}
-      >
-        {state === "collapsed" ? (
-          <div className="flex w-full cursor-pointer items-center justify-center group-has-data-[collapsible=icon]/sidebar-wrapper:-translate-y-[6px]">
-            <h1 className="text-primary block font-serif group-hover/workspace-header:hidden">
-              DF
-            </h1>
-            <SidebarTrigger className="hidden pl-2 group-hover/workspace-header:block" />
+    <div
+      className={cn(
+        "group/workspace-header flex h-15 flex-col justify-center",
+        className,
+      )}
+    >
+      {state === "collapsed" ? (
+        <div className="flex w-full cursor-pointer items-center justify-center group-has-data-[collapsible=icon]/sidebar-wrapper:-translate-y-[6px]">
+          <div className="text-primary block font-serif group-hover/workspace-header:hidden">
+            DF
           </div>
-        ) : (
-          <div className="flex items-center justify-between gap-2">
-            <h1 className="text-primary ml-2 font-serif">DeerFlow</h1>
-            <SidebarTrigger />
-          </div>
-        )}
-      </div>
-    </Link>
+          <SidebarTrigger className="hidden pl-2 group-hover/workspace-header:block" />
+        </div>
+      ) : (
+        <div className="flex items-center justify-between gap-2">
+          <Link href="/workspace" className="text-primary ml-2 font-serif">
+            DeerFlow
+          </Link>
+          <SidebarTrigger />
+        </div>
+      )}
+    </div>
   );
 }
