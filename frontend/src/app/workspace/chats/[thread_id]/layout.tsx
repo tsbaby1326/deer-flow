a8be79ce9bab5891a65f009063d93e48863622cc
tsbaby1326 <imported-history@users.noreply.github.com>
2026-01-21 10:35:50 +0800

    feat: adjust colors

--- frontend/src/app/workspace/chats/[thread_id]/layout.tsx
+++ frontend/src/app/workspace/chats/[thread_id]/layout.tsx
@@ -26,16 +26,20 @@ export default function ChatLayout({
             className="absolute inset-0 z-0 translate-y-[2vh] mask-center mask-no-repeat"
             squareSize={4}
             gridGap={4}
-            color={currentTheme === "dark" ? "#60A5FA" : "#444"}
-            maxOpacity={currentTheme === "dark" ? 0.04 : 0.035}
+            color={
+              currentTheme === "dark" ? "#60A5FA" : "oklch(0 0.0098 87.47)"
+            }
+            maxOpacity={currentTheme === "dark" ? 0.04 : 0.03}
             flickerChance={0.1}
           />
           <FlickeringGrid
             className="absolute inset-0 z-0 translate-y-[2vh] mask-[url(/images/deer.svg)] mask-size-[100vw] mask-center mask-no-repeat md:mask-size-[72vh]"
             squareSize={4}
             gridGap={4}
-            color={currentTheme === "dark" ? "#60A5FA" : "#444"}
-            maxOpacity={currentTheme === "dark" ? 0.15 : 0.13}
+            color={
+              currentTheme === "dark" ? "#60A5FA" : "oklch(0 0.0098 87.47)"
+            }
+            maxOpacity={currentTheme === "dark" ? 0.15 : 0.11}
             flickerChance={0.12}
           />
         </>
