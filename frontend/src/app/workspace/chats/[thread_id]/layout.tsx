66689c8fadf0f186b501277e4e779348683415f7
tsbaby1326 <imported-history@users.noreply.github.com>
2026-01-24 18:48:35 +0800

    feat: remove background

--- frontend/src/app/workspace/chats/[thread_id]/layout.tsx
+++ frontend/src/app/workspace/chats/[thread_id]/layout.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import { useTheme } from "next-themes";
-import { useMemo } from "react";
-
-import { FlickeringGrid } from "@/components/ui/flickering-grid";
 import { ArtifactsProvider } from "@/components/workspace/artifacts";
 
 export default function ChatLayout({
@@ -12,38 +7,5 @@ export default function ChatLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const isNewThread = useMemo(() => {
-    return pathname === "/workspace/chats/new";
-  }, [pathname]);
-  const { resolvedTheme } = useTheme();
-  return (
-    <ArtifactsProvider>
-      {isNewThread && (
-        <>
-          <FlickeringGrid
-            className="absolute inset-0 z-0 translate-y-[2vh] mask-center mask-no-repeat"
-            squareSize={4}
-            gridGap={4}
-            color={
-              resolvedTheme === "dark" ? "#60A5FA" : "oklch(0 0.0098 87.47)"
-            }
-            maxOpacity={resolvedTheme === "dark" ? 0.04 : 0.015}
-            flickerChance={0.1}
-          />
-          <FlickeringGrid
-            className="absolute inset-0 z-0 translate-y-[2vh] mask-[url(/images/deer.svg)] mask-size-[100vw] mask-center mask-no-repeat md:mask-size-[72vh]"
-            squareSize={4}
-            gridGap={4}
-            color={
-              resolvedTheme === "dark" ? "#60A5FA" : "oklch(0 0.0098 87.47)"
-            }
-            maxOpacity={resolvedTheme === "dark" ? 0.15 : 0.075}
-            flickerChance={0.12}
-          />
-        </>
-      )}
-      {children}
-    </ArtifactsProvider>
-  );
+  return <ArtifactsProvider>{children}</ArtifactsProvider>;
 }
